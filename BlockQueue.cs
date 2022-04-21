26c86712b9f69120c8463636a787466836104bb5
DelgaJ <78287397+DelgaJ@users.noreply.github.com>
2022-04-21 16:20:50 -0400

    Add files via upload

---
+++ BlockQueue.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    public class BlockQueue
+    {
+        private readonly Blocks[] blocks = new Blocks[]
+        {
+            new IBlock(),
+            new JBlock(),
+            new LBlock(),
+            new OBlock(),
+            new SBlock(),
+            new TBlock(),
+            new ZBlock()
+        };
+
+        private readonly Random random = new Random();
+
+        public Blocks NextBlock { get; private set; }
+
+        public BlockQueue()
+        {
+            NextBlock = RandomBlock();
+        }
+
+        private Blocks RandomBlock()
+        {
+            return blocks[random.Next(blocks.Length)];
+        }
+
+        public Blocks GetAndUpdate()
+        {
+            Blocks block = NextBlock;
+
+            do
+            {
+                NextBlock = RandomBlock();
+            }
+            while (block.Id == NextBlock.Id);
+
+            return block;
+        }
+
+    }
+}
